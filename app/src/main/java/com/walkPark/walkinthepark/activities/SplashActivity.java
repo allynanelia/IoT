7c81eea9ba36bb149514190450499e1d80a77961
bslee2015 <bslee.2015@sis.smu.edu.sg>
2018-03-01 18:24:06 +0800

    Push again

---
+++ app/src/main/java/com/walkPark/walkinthepark/activities/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.walkPark.walkinthepark.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.walkinthepark.R;
+import com.walkPark.walkinthepark.WalkInTheParkGson;
+import com.walkPark.walkinthepark.backend.RouteInterface;
+import com.walkPark.walkinthepark.backend.WalkInTheParkRetrofit;
+import com.walkPark.walkinthepark.models.RouteResponse;
+
+import org.jetbrains.annotations.Nullable;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by Boon Sing on 01-Mar-18.
+ */
+
+public class SplashActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },1000);
+    }
+}
